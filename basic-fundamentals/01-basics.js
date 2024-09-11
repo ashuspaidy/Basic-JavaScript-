f08cf8805b1f96f3134826df71b1bdfc15acd000
ashuspaidy <96715678+ashuspaidy@users.noreply.github.com>
2024-09-11 02:13:46 +0000

    changes done

--- basic-fundamentals/01-basics.js
+++ basic-fundamentals/01-basics.js
@@ -1,3 +1,8 @@
 console.log('Running Our Code on Github');
 alert('hurray!!!!!!!!!!!!!');
 // prompt('will take you higher & higher');
+
+const memory = null
+const memory2 = undefined;
+
+console.table(typeof[memory],[memory2]);
